0f78e9873e7be5484c0ac0fd437ab658b4e95982
hondahiroshi <h.honda5427@gmail.com>
2024-04-01 23:22:39 +0900

    create recon

--- simind_study.py
+++ simind_study.py
@@ -1,39 +1,29 @@
 import os
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
 import torch
 from pytomography.io.SPECT import simind
 from pytomography.transforms.SPECT.attenuation import SPECTAttenuationTransform
 from pytomography.transforms.SPECT.psf import SPECTPSFTransform
+from pytomography.projectors.SPECT import SPECTSystemMatrix
+from pytomography.algorithms import OSEM
+from pytomography.likelihoods import PoissonLogLikelihood
+
+colors = np.loadtxt(r'simind_study/pet_colors.txt').reshape(-1,3)/255.0
+custom_cmap = LinearSegmentedColormap.from_list('custom_colormap', colors)
 
 '''ペネトレーションルーチンで作成した各ファイルを読み込み、リスト化する'''
 class SimindStudy:
-    main_header: dict
-    upper_header: dict
-    lower_header: dict
-    calib_header: dict
+    _header: dict
+    _proj: dict[str, dict]
 
-    main_proj: dict[str, dict]       
-    upper_proj: dict[str, dict]
-    lower_proj: dict[str, dict]
-    calib_proj: dict[str, dict]
+    def __init__(self, _dir: str) -> None:
+        self._dir = _dir
+        self._header = self.read_header_data(self._dir)
+        self._proj = self.read_projection_data(self._dir, self._header)
 
-    def __init__(self, main_dir: str, upper_dir: str, lower_dir: str, calib_dir: str) -> None:
-        self.main_dir = main_dir
-        self.upper_dir = upper_dir
-        self.lower_dir = lower_dir
-        self.calib_dir = calib_dir
-
-        self.main_header = self.read_header_data(self.main_dir)
-        self.upper_header = self.read_header_data(self.upper_dir)
-        self.lower_header = self.read_header_data(self.lower_dir)
-        self.calib_header = self.read_header_data(self.calib_dir)
-
-        self.main_proj = self.read_projection_data(self.main_dir, self.main_header)
-        self.upper_proj = self.read_projection_data(self.upper_dir, self.upper_header)
-        self.lower_proj = self.read_projection_data(self.lower_dir, self.lower_header)
-        self.calib_proj = self.read_projection_data(self.calib_dir, self.calib_header)
-        
     # 指定したディレクトリのファイルを読み込み、ファイル名とファイルの辞書を作成
     def read_header_data(self, input_dir: str) -> dict: 
         
@@ -89,15 +79,44 @@ class SimindStudy:
 
         return projections
     
+    def osem_psf(self, n_iters:int, n_subsets:int, proj:torch.Tensor, data:dict, scatter=None) -> torch.Tensor:
+        # att_transform = SPECTAttenuationTransform(data['attenuation_map'])
+        # psf_transform = SPECTPSFTransform(data['psf_meta'])
+        system_matrix = SPECTSystemMatrix(
+            obj2obj_transforms=[data['attenuation_transform'], data['psf_transform']],
+            proj2proj_transforms=[],
+            object_meta=data['object_meta'],
+            proj_meta=data['proj_meta']
+        )
+        likelihood = PoissonLogLikelihood(system_matrix, proj)
+
+        osem = OSEM(likelihood)
+
+        return osem(n_iters, n_subsets)
+    
+    @property
+    def proj(self):
+        return self._proj
+    
 main_dir = 'C:\\Users\\hhond\\source\\repos\\simind_study\\lehr_main'
 upper_dir = 'C:\\Users\\hhond\\source\\repos\\simind_study\\lehr_up'
 lower_dir = 'C:\\Users\\hhond\\source\\repos\\simind_study\\lehr_low'
 calib_dir = 'C:\\Users\\hhond\\source\\repos\\simind_study\\lehr_cali'
 
-lehr_study = SimindStudy(main_dir, upper_dir, lower_dir, calib_dir)
 
+lehr_main = SimindStudy(main_dir)
 activity = 150
 dT = 20
 
-main = lehr_study.main_proj
-
+main = lehr_main.proj
+b1 = main['lehr_main.b01']
+b1_proj = b1['projections']
+b1_att_map = b1['attenuation_map']
+b1_proj_noise = torch.poisson(b1_proj * activity * dT)
+b1_ax = lehr_main.osem_psf(6, 4, b1_proj_noise, b1, None)
+b1_ax = b1_ax[0].cpu().numpy()
+plt.pcolormesh(b1_att_map[0,:,:,128].cpu().T, cmap='Greys_r')
+plt.pcolormesh(b1_ax[:,:,128].T, cmap=custom_cmap, alpha=0.8)
+plt.colorbar(label='Counts')
+plt.axis('off')
+plt.show()
